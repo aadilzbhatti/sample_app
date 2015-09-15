f726b487086aa0cbbe7314bc2d9164bbc6b6463a
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-09-15 16:17:42 -0500

    finished updating/deleting users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:aadil)
+    @admin = users(:aadil)
+    @non_admin = users(:archer)
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
